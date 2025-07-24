ae1589cc5db6fc4cfd552e2c253b4f43bcdf37e5
henrypalm <rgoodwin130@gmail.com>
2025-07-24 13:29:05 -0400

    itemPrices.js and itemColors.js files added

---
+++ itemColors.js
@@ -0,0 +1,16 @@
+const itemColors = {
+  "Stileron": "#FFD700",
+  "Quantanium": "#FFD700",
+  "Riccite": "#FFD700",
+  "Taranite": "#006400",
+  "Bexalite": "#006400",
+  "Gold": "#006400",
+  "Diamond": "#006400",
+  "Borase": "#0000FF",
+  "Laranite": "#0000FF",
+  "Beryl": "#0000FF",
+  "Agricium": "#0000FF",
+  "Hephaestanite": "#0000FF",
+  "Recycled Material Composite": "#FFD700",
+  "Construction Material": "#FF0000"
+};
